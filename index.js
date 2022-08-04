128f5172a4095af00dbb768dbdc6b425d412ea4c
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-05 00:02:35 +0100

    log() method was used in calling the emitter.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // const person = require('./person');
-const Person = require('./person');
+// const Person = require('./person');
 
-const person1 = new Person('John Doe', 30);
+// const person1 = new Person('John Doe', 30);
 
-person1.greeting();
-
-
+// person1.greeting();
 
 // console.log(person.name);
+
+
+const Logger = require('./logger.js');
+const logger =  new Logger();
+logger.on('message', data => console.log('Called Listener', data));
+
+// * Calling the log() method
+logger.log('Ibraheem Omikunle');
+logger.log('Ibrahe');
+logger.log('mikunle');
+logger.log('Ibrmikunle');
