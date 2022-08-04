2c054aff1c9bfd18931b9f1296c065cdac5b4707
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-04 16:00:48 +0100

    [os_module] was completed.

---
+++ core/os_module.js
@@ -0,0 +1,30 @@
+const os  = require('os');
+
+// *Get  Platform [operating systems]
+console.log(os.platform());
+
+
+// * CPU architechture
+console.log(os.arch())
+
+
+// * CPU Core Info
+console.log(os.cpus())
+
+
+// * Free memory
+console.log(os.freemem());
+
+
+// * Total memory
+console.log(os.totalmem());
+
+// * Home directory
+console.log(os.homedir());
+
+
+
+// * Uptime -> the amount of time that your system has been up.
+console.log(os.uptime());
+
+
