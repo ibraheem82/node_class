40b7431237156ddcb9d75c1e0159401f0bf2dffd
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-05 03:51:01 +0100

    everything was commented

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
 // console.log(person.name);
 
 
-const Logger = require('./logger.js');
-const logger =  new Logger();
-logger.on('message', data => console.log('Called Listener', data));
+// const Logger = require('./logger.js');
+// const logger =  new Logger();
+// logger.on('message', data => console.log('Called Listener', data));
 
 // * Calling the log() method
-logger.log('Ibraheem Omikunle');
-logger.log('Ibrahe');
-logger.log('mikunle');
-logger.log('Ibrmikunle');
+// logger.log('Ibraheem Omikunle');
+// logger.log('Ibrahe');
+// logger.log('mikunle');
+// logger.log('Ibrmikunle');
